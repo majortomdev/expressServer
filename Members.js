8093347ca4e37703ff8d51b9ca50e3a1f57f9bc4
Joseph Kinahan <kinahanj25@gmail.com>
2020-03-31 22:43:24 +0100

    basic bones on an express server....

---
+++ Members.js
@@ -0,0 +1,22 @@
+const members = [
+    {
+        id: 1,
+        name: 'Peter Dalmarris',
+        email: 'pdm123@gmail.com',
+        status: 'active'
+    }, 
+    {
+        id: 2,
+        name: 'Shirley McClaine',
+        email: 'smcsmc@hgotmail.com',
+        status: 'inactive'
+    }, 
+    {
+        id: 3,
+        name: 'Dalton Dangler',
+        email: 'pdiddy23@gmail.com',
+        status: 'active'
+    }
+];
+
+module.exports = members;

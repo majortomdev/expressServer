4bc339d913ac0782715d582ec26d7915fc93dac5
Joseph Kinahan <kinahanj25@gmail.com>
2020-04-01 18:19:15 +0100

    added handlebars create the view/ FE

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
- 
-
+const exphbs = require('express-handlebars'); 
 const logger = require('./middleware/Logger');
+const members = require('./Members');
 
 const app = express();
 
@@ -10,12 +10,24 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
+
+
+//route for new homepage
+app.get('/', (req,res) => res.render('index', {
+    title: 'Member Application',
+    members
+}));
+// handlebars middleare
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
 //initialise middleware
 app.use(logger);
 
-
 //set static folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+
 
 app.use('/api/members', require('./routes/api/members'))
 
--- routes/api/members.js
+++ routes/api/members.js
@@ -27,7 +27,8 @@ router.post('/', (req,res) => {
     }
 
     members.push(newMember);
-    res.json(members);
+    //res.json(members);
+    res.redirect('/');
 });
 
 //update members details
